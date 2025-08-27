b36654bb551864fe4fabe84f40c161e2539b30b1
Felipe Quezada <fequezad@ucsc.edu>
2025-08-27 17:36:20 -0400

    update

---
+++ R/environmental_data.R
@@ -0,0 +1,12 @@
+###----------------------------------------------###
+###           Environmental covariates           ### 
+###----------------------------------------------###
+
+# Loack packages
+library(CopernicusMarine)
+
+
+cms_set_username("fquezadaescalon")
+cms_set_password("Pgpd5858!")
+
+
