a4d20dbdaeb18c190a7a2e7a5e419bb299b8a2c2
Felipe Quezada <fequezad@ucsc.edu>
2025-08-28 12:31:10 -0400

    update

--- R/load_environmental_data.R
+++ R/load_environmental_data.R
@@ -1,17 +1,66 @@
-# Install if not yet
-install.packages("ncdf4")
-
-# Load package
+library(dplyr)
 library(ncdf4)
+library(data.table)
+library(lubridate)
 
-# Open NetCDF file
-ncfile <- nc_open("C:/GitHub/Impact of Environment on Harvest/data/env/cmems_mod_glo_phy_my_0.083deg_P1M-m_so-thetao-uo-vo_78.00W-71.50W_42.00S-32.00S_0.49-1.54m_2012-01-01-2021-06-01.nc")
+f <- "C:/GitHub/Impact of Environment on Harvest/data/env/cmems_mod_glo_phy_my_0.083deg_P1M-m_so-thetao-uo-vo_78.00W-71.50W_42.00S-32.00S_0.49-1.54m_2012-01-01-2021-06-01.nc"
 
-# Print metadata (variables, dimensions, attributes)
-print(ncfile)
+nc <- ncdf4::nc_open(f)
 
-# Example: extract variable "thetao" (temperature)
-thetao <- ncvar_get(ncfile, "thetao")
+## Dimensions ##
+lon <- ncvar_get(nc, "longitude")
+lat <- ncvar_get(nc, "latitude")
+tim <- ncvar_get(nc, "time")
+dep <- ncvar_get(nc, "depth")
 
-# Close file when done
-nc_close(ncfile)
+## Vars ##
+var_names <- names(nc$var)
+so <- ncvar_get(nc, "so")
+sst <- ncvar_get(nc, "thetao")
+uo <- ncvar_get(nc, "uo")
+vo <- ncvar_get(nc, "vo")
+
+ncdf4::nc_close(nc)
+
+dimnames(so)  <- list(lon = lon, lat = lat, dep = dep, time = tim)
+so_dt <- as.data.table(data.table::melt(so, value.name = "so"))
+setnames(so_dt, c("lon", "lat", "depth", "time", "so"))
+so_dt[, date := as.Date("1950-01-01") + time / 24]
+so_dt[, time := NULL]
+
+dimnames(sst)  <- list(lon = lon, lat = lat, dep = dep, time = tim)
+sst_dt <- as.data.table(data.table::melt(sst, value.name = "sst"))
+setnames(sst_dt, c("lon", "lat", "depth", "time", "sst"))
+sst_dt[, date := as.Date("1950-01-01") + time / 24]
+sst_dt[, time := NULL]
+
+dimnames(uo)  <- list(lon = lon, lat = lat, dep = dep, time = tim)
+uo_dt <- as.data.table(data.table::melt(uo, value.name = "uo"))
+setnames(uo_dt, c("lon", "lat", "depth", "time", "uo"))
+uo_dt[, date := as.Date("1950-01-01") + time / 24]
+uo_dt[, time := NULL]
+
+dimnames(vo)  <- list(lon = lon, lat = lat, dep = dep, time = tim)
+vo_dt <- as.data.table(data.table::melt(vo, value.name = "vo"))
+setnames(vo_dt, c("lon", "lat", "depth", "time", "vo"))
+vo_dt[, date := as.Date("1950-01-01") + time / 24]
+vo_dt[, time := NULL]
+
+
+dt <- Reduce(function(x, y) merge(x, y, by = c("lon", "lat", "depth", "date")),
+             list(so_dt, sst_dt, uo_dt, vo_dt)) %>% 
+  filter(depth < 1) %>%
+  mutate(current_speed = sqrt(uo^2 + vo^2),
+         current_direction = atan2(vo, uo) * 180 / pi) %>% 
+  select(-c("depth", "uo", "vo")) %>%
+  mutate(bad_weather = ifelse(current_speed > 1, 1, 0))
+
+
+dt[, `:=`(
+  year  = year(date),
+  month = month(date)
+)]
+
+
+
+
