dcb55a582fd785fbfe90ce6933c8a773cdf2d2b9
Felipe Quezada <fequezad@ucsc.edu>
2025-09-02 09:51:15 -0400

    Update load_environmental_data_Wind.R

--- R/load_environmental_data_Wind.R
+++ R/load_environmental_data_Wind.R
@@ -14,30 +14,6 @@ usuario <- Sys.info()[["user"]]
 dirdata <- paste0("C:/Users/", paste0(usuario, "/OneDrive - Universidad de Concepción/FONDECYT Iniciacion/Data/"))
 f <- paste0(dirdata, "Environmental/cmems_obs-wind_glo_phy_my_l4_0.125deg_PT1H_multi-vars_77.94W-71.56W_41.94S-32.06S_2012-01-01-2025-04-19.nc")
 
-
-## Open NCDF4 file
-
-nc <- ncdf4::nc_open(f)
-
-## Dimensions ##
-lon <- ncvar_get(nc, "longitude")
-lat <- ncvar_get(nc, "latitude")
-tim <- ncvar_get(nc, "time")
-
-## Vars ##
-var_names <- names(nc$var)
-ua <- ncvar_get(nc, "eastward_wind")
-va <- ncvar_get(nc, "northward_wind")
-
-ncatt_get(nc, "time", "units")
-ncdf4::nc_close(nc)
-
-# --- Convert hours → day index
-
-
-library(ncdf4)
-library(data.table)
-
 # --- Open file
 nc <- nc_open(f)
 
@@ -51,50 +27,48 @@ va <- ncvar_get(nc, "northward_wind")
 time_units <- ncatt_get(nc, "time", "units")$value
 nc_close(nc)
 
-# --- Time conversion
-dates <- as.Date(as.POSIXct(tim, origin = "1990-01-01", tz = "UTC"))
-day_index <- as.integer(dates - min(dates))
-unique_dates <- unique(dates)
+# --- Convert time
+origin_str <- sub("seconds since ", "", time_units)
+dates <- as.Date(as.POSIXct(tim, origin = origin_str, tz="UTC"))
 
-# --- Compute wind speed + direction
-wind_speed <- sqrt(ua^2 + va^2)
-wind_direction <- atan2(ua, va) * 180/pi
-wind_direction[wind_direction < 0] <- wind_direction[wind_direction < 0] + 360
+# --- Expand to vectors
+grid <- CJ(lon=lon, lat=lat, time=seq_along(tim))   # fast cross join
+grid[, date := dates[time]]
 
-# --- Function for daily stats
-day_stats <- function(x, idx) {
-  tapply(x, idx, function(v) c(mean=mean(v, na.rm=TRUE),
-                               min=min(v, na.rm=TRUE),
-                               max=max(v, na.rm=TRUE)))
-}
+# Fill values
+grid[, ua := as.vector(ua)]
+grid[, va := as.vector(va)]
 
-# --- Aggregate daily
-wind_speed_day <- apply(wind_speed, c(1,2), day_stats, idx=day_index)
-wind_speed_day <- aperm(wind_speed_day, c(1,2,4,3))
-dimnames(wind_speed_day) <- list(lon=lon, lat=lat, date=unique_dates,
-                            stat=c("mean","min","max"))
+# --- Compute derived vars
+grid[, speed := sqrt(ua^2 + va^2)]
+grid[, direction := atan2(ua, va) * 180/pi]
+grid[direction < 0, direction := direction + 360]
 
-wind_direction_day <- apply(wind_direction, c(1,2), day_stats, idx=day_index)
-wind_direction_day <- aperm(wind_direction_day, c(1,2,4,3))
-dimnames(wind_direction_day) <- list(lon=lon, lat=lat, date=unique_dates,
-                                stat=c("mean","min","max"))
+# --- Daily aggregation
+wind_dt <- grid[, .(
+  ua_mean  = mean(ua, na.rm=TRUE),
+  ua_min   = min(ua, na.rm=TRUE),
+  ua_max   = max(ua, na.rm=TRUE),
+  va_mean  = mean(va, na.rm=TRUE),
+  va_min   = min(va, na.rm=TRUE),
+  va_max   = max(va, na.rm=TRUE),
+  speed_mean = mean(speed, na.rm=TRUE),
+  speed_min  = min(speed, na.rm=TRUE),
+  speed_max  = max(speed, na.rm=TRUE),
+  # circular mean of direction:
+  dir_mean = atan2(mean(sin(direction*pi/180), na.rm=TRUE),
+                   mean(cos(direction*pi/180), na.rm=TRUE)) * 180/pi,
+  dir_min  = min(direction, na.rm=TRUE),
+  dir_max  = max(direction, na.rm=TRUE)
+), by=.(lon, lat, date)]
 
-# --- Convert to data.table
-speed_dt <- as.data.table(reshape2::melt(wind_speed_day, value.name="wind_speed"))
-speed_dt <- dcast(speed_dt, lon + lat + date ~ stat, value.var="wind_speed")
-setnames(speed_dt, c("mean","min","max"), c("wind_speed_mean","wind_speed_min","wind_speed_max"))
+# Normalize circular mean to [0,360)
+wind_dt[dir_mean < 0, dir_mean := dir_mean + 360]
 
-direction_dt <- as.data.table(reshape2::melt(wind_direction_day, value.name="wind_direction"))
-direction_dt <- dcast(direction_dt, lon + lat + date ~ stat, value.var="wind_direction")
-setnames(direction_dt, c("mean","min","max"),
-         c("wind_dir_mean","wind_dir_min","wind_dir_max"))
-
-# --- Merge
-wind_dt <- merge(speed_dt, direction_dt, by=c("lon","lat","date"))
-
-# --- Add year & month
+# --- Add year/month
 wind_dt[, year  := year(date)]
 wind_dt[, month := month(date)]
 
-saveRDS(dt, file = "data/env/WindDaily_2012_2025.rds")
+
+saveRDS(wind_dt, file = "data/env/WindDaily_2012_2025.rds")
 
