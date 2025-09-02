9d5c1bf51247b5773ae1097fad5bd62dff33690a
Felipe Quezada <fequezad@ucsc.edu>
2025-09-02 10:43:20 -0400

    Create load_environmental_data_MERGE.R

---
+++ R/load_environmental_data_MERGE.R
@@ -0,0 +1,11 @@
+###----------------------------------------------###
+###   Environmental covariates: Merge ### 
+###----------------------------------------------###
+
+rm(list = ls())
+gc()
+
+library(dplyr)
+library(data.table)
+library(lubridate)
+
