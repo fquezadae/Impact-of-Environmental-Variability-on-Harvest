40e6a1be4cb2c96d2f6ae4ce26e759bbf6db7d9e
Felipe Quezada <fequezad@ucsc.edu>
2025-09-08 17:00:52 -0300

    update

---
+++ R/logbook_data.r
@@ -0,0 +1,68 @@
+
+
+###----------------------------------------------###
+###              Logbook data                   ### 
+###----------------------------------------------###
+
+rm(list = ls())
+gc()
+
+# Define directory
+
+usuario <- Sys.info()[["user"]]
+# computador <- Sys.info()[["nodename"]]  # Alternativamente puedes usar esto
+if (usuario == "felip") {
+  dirdata <- "C:/Users/felip/OneDrive - Universidad de Concepción/FONDECYT Iniciacion/Data/"
+} else if (usuario == "FACEA") {
+  dirdata <- "C:/Users/FACEA/OneDrive - Universidad de Concepción/FONDECYT Iniciacion/Data/"
+} else {
+  stop("Usuario no reconocido. Defina el directorio correspondiente.")
+}
+rm(usuario)
+
+# Load packages 
+
+library(readxl)
+library(tidyverse)
+
+# library(knitr)
+# library(kableExtra)
+# library(ggalluvial)
+# library(purrr)
+# library(stringr)
+# library(readxl)
+
+### Load logbooks information for central-south Chile
+logbooks <- 
+  read_excel(paste0(dirdata, "IFOP/1. BITACORA CENTRO SUR.xlsx"), 
+             sheet = "bIt_2001_2024_comercial") %>%
+  filter(REGION %in% c(5,6,7,8,9,10,14,16))
+
+### Get species name in logbooks
+species <- 
+  read_excel(
+    paste0(dirdata, "IFOP/1. BITACORA CENTRO SUR.xlsx"), 
+    sheet = "PESQUERIAS_MAESTRO_ESPECIE") %>%
+  dplyr::select(c('COD_ESPECIE', 'NOMBRE_ESPECIE'))
+logbooks <- left_join(logbooks, species, by = "COD_ESPECIE") 
+logbooks$year <- year(logbooks$FECHA_HORA_RECALADA)
+logbooks$month <- month(logbooks$FECHA_HORA_RECALADA)  
+rm(list = c("species"))
+
+
+
+
+### Check longitude by latitude
+
+# logbooks_filtered <- logbooks %>%
+#   filter(year > 2011) %>%
+#   filter(NOMBRE_ESPECIE %in% c("JUREL")) %>%
+#   group_by(LATITUD) %>%
+#   summarize(mean_long = mean(LONGITUD, na.rm = TRUE)) %>%
+#   arrange(LATITUD)  # Ordena por latitud ascendente
+# 
+# # Ejemplo de plot
+# ggplot(logbooks_filtered, aes(x = LATITUD, y = mean_long)) +
+#   geom_line()
+
+
--- data/env/download.py
+++ data/env/download.py
@@ -3,16 +3,16 @@ mamba activate C:\Users\FACEA\miniforge3\envs\cmems_env_py3.12
 
 
 ## GLORYS Monthly
-copernicusmarine subset --dataset-id cmems_mod_glo_phy_my_0.083deg_P1M-m --variable so --variable thetao --variable uo --variable vo --start-datetime 2012-01-01T00:00:00 --end-datetime 2021-06-01T00:00:00 --minimum-longitude -78 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956 
-copernicusmarine subset --dataset-id cmems_mod_glo_phy_myint_0.083deg_P1M-m --variable so --variable thetao --variable uo --variable vo --start-datetime 2021-07-01T00:00:00 --end-datetime 2025-06-01T00:00:00 --minimum-longitude -78 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956
+copernicusmarine subset --dataset-id cmems_mod_glo_phy_my_0.083deg_P1M-m --variable so --variable thetao --variable uo --variable vo --start-datetime 2012-01-01T00:00:00 --end-datetime 2021-06-01T00:00:00 --minimum-longitude -81 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956 
+copernicusmarine subset --dataset-id cmems_mod_glo_phy_myint_0.083deg_P1M-m --variable so --variable thetao --variable uo --variable vo --start-datetime 2021-07-01T00:00:00 --end-datetime 2025-06-01T00:00:00 --minimum-longitude -81 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956
 
 ## GLORYS Daily
-copernicusmarine subset --dataset-id cmems_mod_glo_phy_my_0.083deg_P1D-m    --variable so --variable thetao --variable uo --variable vo --start-datetime 2012-01-01T00:00:00 --end-datetime 2021-06-30T00:00:00 --minimum-longitude -78 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956 
-copernicusmarine subset --dataset-id cmems_mod_glo_phy_myint_0.083deg_P1D-m --variable so --variable thetao --variable uo --variable vo --start-datetime 2021-07-01T00:00:00 --end-datetime 2025-06-28T00:00:00 --minimum-longitude -78 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956
+copernicusmarine subset --dataset-id cmems_mod_glo_phy_my_0.083deg_P1D-m    --variable so --variable thetao --variable uo --variable vo --start-datetime 2012-01-01T00:00:00 --end-datetime 2021-06-30T00:00:00 --minimum-longitude -81 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956 
+copernicusmarine subset --dataset-id cmems_mod_glo_phy_myint_0.083deg_P1D-m --variable so --variable thetao --variable uo --variable vo --start-datetime 2021-07-01T00:00:00 --end-datetime 2025-06-28T00:00:00 --minimum-longitude -81 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32 --minimum-depth 0.49402499198913574 --maximum-depth 1.5413750410079956
 
 
 ## Wind 
-copernicusmarine subset --dataset-id cmems_obs-wind_glo_phy_my_l4_0.125deg_PT1H --variable eastward_wind --variable northward_wind --start-datetime 2012-01-01T00:00:00 --end-datetime 2025-04-19T23:59:00 --minimum-longitude -78 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32
+copernicusmarine subset --dataset-id cmems_obs-wind_glo_phy_my_l4_0.125deg_PT1H --variable eastward_wind --variable northward_wind --start-datetime 2012-01-01T00:00:00 --end-datetime 2025-04-19T23:59:00 --minimum-longitude -81 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32
 
 ## Chlorophyll
-copernicusmarine subset --dataset-id cmems_obs-oc_glo_bgc-plankton_my_l4-gapfree-multi-4km_P1D --variable CHL --start-datetime 2012-01-01T00:00:00 --end-datetime 2025-08-20T00:00:00 --minimum-longitude -78 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32
+copernicusmarine subset --dataset-id cmems_obs-oc_glo_bgc-plankton_my_l4-gapfree-multi-4km_P1D --variable CHL --start-datetime 2012-01-01T00:00:00 --end-datetime 2025-08-20T00:00:00 --minimum-longitude -81 --maximum-longitude -71.5 --minimum-latitude -42 --maximum-latitude -32
