0e0d565f56f02de68af86706ecddb8e606110476
Felipe J. Quezada-Escalona <felipequezada@udec.cl>
2026-04-30 16:21:55 -0400

    update slides and paper

--- knit.R
+++ knit.R
@@ -1,5 +1,7 @@
 Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/bin/tools")
 
+if (rstudioapi::isAvailable()) rstudioapi::documentSaveAll()
+
 # Paper 1: Climate projections (biomass + effort)
 rmarkdown::render(here::here("paper1/paper1_climate_projections.Rmd"))
 
