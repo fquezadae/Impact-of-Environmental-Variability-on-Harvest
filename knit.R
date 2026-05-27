5a681e9abfb53234c82ffd67e88dd3ebfb8d2a09
Felipe J. Quezada-Escalona <felipequezada@udec.cl>
2026-05-27 10:41:27 -0400

    MRE submission-ready: trim main to 42pp, split Online Appendix (29pp), MRE formatting

--- knit.R
+++ knit.R
@@ -2,11 +2,10 @@ Sys.setenv(RSTUDIO_PANDOC = "C:/Program Files/RStudio/resources/app/bin/quarto/b
 
 if (rstudioapi::isAvailable()) rstudioapi::documentSaveAll()
 
-# 1. Main manuscript (paper1)
+# paper1
 rmarkdown::render("paper1/paper1_climate_projections.Rmd")
-
-# 2. Online Appendix (supplementary materials)
 rmarkdown::render("paper1/paper1_supplementary_materials.Rmd")
+rmarkdown::render("paper1/cover_letter.md")
 
 # Paper 2: Bioeconomic optimization (when ready)
 # rmarkdown::render(here::here("paper2/paper2_bioeconomic_optimization.Rmd"))
